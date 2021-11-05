bd170a9b5dec383d3ea8383bb62e98b5e7307e72
SardorAbduxoliqov <sardornewgithub@mail.ru>
2021-11-05 21:11:22 +0500

    meta and user added

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = "https://shopalloma.herokuapp.com/api/";
 const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNzQxMTYyNzFmYzE4NTYzNTY2NGQ5MiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNTg0NDgyNCwiZXhwIjoxNjM2MTA0MDI0fQ.K3-AuitGZoIriImV-tWOyy49N54p6ZL335XKDXIWOe4" ||
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNzI5M2YxMDE3MDc3OTljNjdiNGNhOSIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2MzQ5ODYwODksImV4cCI6MTYzNTI0NTI4OX0.3e4uV-rCDTADyaIMJf8gq9dm2_xo0Dl2pxQWidYOfeg" ||
   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
     .accessToken;
 
