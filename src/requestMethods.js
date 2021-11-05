c827485dd8f4eeb463ad8fd6e890b87438bd3d61
SardorAbduxoliqov <sardornewgithub@mail.ru>
2021-11-05 22:16:40 +0500

    token reuse

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const BASE_URL = "https://shopalloma.herokuapp.com/api/";
-const TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNzI5M2YxMDE3MDc3OTljNjdiNGNhOSIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2MzQ5ODYwODksImV4cCI6MTYzNTI0NTI4OX0.3e4uV-rCDTADyaIMJf8gq9dm2_xo0Dl2pxQWidYOfeg" || JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
-.currentUser.accessToken;
+const TOKEN =
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNzQxMTYyNzFmYzE4NTYzNTY2NGQ5MiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNjEyOTc4OSwiZXhwIjoxNjM2Mzg4OTg5fQ.fZJ1Df3dRQLeFS8RTXnRZaTdi2BDWM8mgAAQ13myJDw" ||
+  JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
+    .accessToken;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
