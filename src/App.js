a5c654b8f020e4990b38230923050ad03c4fd434
SardorAbduxoliqov <sardornewgithub@mail.ru>
2021-11-05 22:42:59 +0500

    token reuse

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import NewProduct from "./pages/newProduct/NewProduct";
 import Login from "./pages/login/Login";
 function App() {
   const admin =
+    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNzQxMTYyNzFmYzE4NTYzNTY2NGQ5MiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNjEyOTc4OSwiZXhwIjoxNjM2Mzg4OTg5fQ.fZJ1Df3dRQLeFS8RTXnRZaTdi2BDWM8mgAAQ13myJDw" ||
     JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
       .currentUser.isAdmin;
   return (
