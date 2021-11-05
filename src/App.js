295c16f33c56f7ee53db4661e1fcff45981a3593
SardorAbduxoliqov <sardornewgithub@mail.ru>
2021-11-05 21:25:02 +0500

    client done

--- src/App.js
+++ src/App.js
@@ -11,9 +11,10 @@ import Product from "./pages/product/Product";
 import NewProduct from "./pages/newProduct/NewProduct";
 import Login from "./pages/login/Login";
 function App() {
-  const admin =  JSON.parse(
-    JSON.parse(localStorage.getItem("persist:root")).user
-  ).currentUser.isAdmin;;     
+  const admin =
+    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNzI5M2YxMDE3MDc3OTljNjdiNGNhOSIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2MzQ5ODYwODksImV4cCI6MTYzNTI0NTI4OX0.3e4uV-rCDTADyaIMJf8gq9dm2_xo0Dl2pxQWidYOfeg" ||
+    JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
+      .currentUser.isAdmin;
   return (
     <Router>
       <Switch>
