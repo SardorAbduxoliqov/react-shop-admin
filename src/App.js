6b2ed859091be7bb20ee064d0aab17e724e83d5b
SardorAbduxoliqov <sardornewgithub@mail.ru>
2021-11-05 22:02:29 +0500

    useselector

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import ProductList from "./pages/productList/ProductList";
 import Product from "./pages/product/Product";
 import NewProduct from "./pages/newProduct/NewProduct";
 import Login from "./pages/login/Login";
+import { useSelector } from "react-redux";
 function App() {
   const admin = useSelector((state) => state.user.currentUser);
   return (
